a7bd82cb408dac0c752f6936a42329303b6e7938
Mattie Malling <mattsam1234@gmail.com>
2024-03-26 21:30:25 +0000

    final sql query cleanup

--- data_queries.sql
+++ data_queries.sql
@@ -151,3 +151,26 @@ SELECT year,
         )::FLOAT
     ) AS actual_time_taken
 FROM average_time;
+WITH cte AS(
+    SELECT TO_TIMESTAMP(
+            CONCAT(year, '-', month, '-', day, ' ', timestamp),
+            'YYYY-MM-DD HH24:MI:SS'
+        ) as datetimes,
+        year
+    FROM dim_date_times
+    ORDER BY datetimes DESC
+),
+cte2 AS(
+    SELECT year,
+        datetimes,
+        LEAD(datetimes, 1) OVER (
+            ORDER BY datetimes DESC
+        ) as time_difference
+    FROM cte
+)
+SELECT year,
+    AVG((datetimes - time_difference)) as actual_time_taken
+FROM cte2
+GROUP BY year
+ORDER BY actual_time_taken DESC
+LIMIT 5;
